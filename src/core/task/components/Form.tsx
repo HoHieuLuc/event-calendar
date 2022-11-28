6c53ca0edc19ed7afa63f3811d69537649e76f1b
Ho Hieu Luc <kingofika123@gmail.com>
2022-11-28 21:53:49 +0700

    init tiptap

--- src/core/task/components/Form.tsx
+++ src/core/task/components/Form.tsx
@@ -1,27 +1,59 @@
 import {
-    Button,
-    Center,
+    Box,
     Checkbox,
     ColorInput,
+    Group,
+    Input,
+    Paper,
     SimpleGrid,
     Stack,
+    Switch,
     TextInput,
 } from '@mantine/core';
 import { DatePicker, TimeInput } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { showNotification } from '@mantine/notifications';
+import { Link } from '@mantine/tiptap';
+import Color from '@tiptap/extension-color';
+import Highlight from '@tiptap/extension-highlight';
+import Placeholder from '@tiptap/extension-placeholder';
+import SubScript from '@tiptap/extension-subscript';
+import Superscript from '@tiptap/extension-superscript';
+import TextAlign from '@tiptap/extension-text-align';
+import TextStyle from '@tiptap/extension-text-style';
+import Underline from '@tiptap/extension-underline';
+import { useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { useState } from 'react';
 
 import { getExactHour, mergeDateAndTime } from '~/lib/dates';
 
 import { Task, TaskForm } from '../task.type';
+import RTE from './RTE';
 
 interface Props {
     onSubmit: (values: Omit<Task, 'id'>, callback?: () => void) => void;
-    loading: boolean;
     initialValues?: TaskForm;
+    formBottomSection: React.ReactNode;
 }
 
-const Form = ({ onSubmit, initialValues, loading }: Props) => {
+const Form = ({ onSubmit, initialValues, formBottomSection }: Props) => {
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Underline,
+            Link,
+            Superscript,
+            SubScript,
+            Highlight,
+            TextStyle,
+            Color,
+            TextAlign.configure({ types: ['heading', 'paragraph'] }),
+            Placeholder.configure({ placeholder: 'Content' }),
+        ],
+        content: initialValues?.content || '<p></p>',
+    });
+
     const taskForm = useForm<TaskForm, (values: TaskForm) => Omit<Task, 'id'>>({
         initialValues: initialValues || {
             title: '',
@@ -31,6 +63,7 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
             finishTime: getExactHour(9),
             color: '',
             done: false,
+            content: '',
         },
         validate: {
             title: (value) => (value ? null : 'Title is required'),
@@ -44,8 +77,11 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
             done: values.done,
             start: mergeDateAndTime(values.startDate, values.startTime),
             end: mergeDateAndTime(values.finishDate, values.finishTime),
+            content: editor?.getHTML() || '',
         }),
     });
+
+    const [previewMode, setPreviewMode] = useState(false);
 
     const submit = (values: Omit<Task, 'id'>) => {
         if (values.start.getTime() >= values.end.getTime()) {
@@ -62,11 +98,35 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
     return (
         <form onSubmit={taskForm.onSubmit(submit)}>
             <Stack>
-                <TextInput
-                    label='Title'
-                    placeholder='Title'
-                    {...taskForm.getInputProps('title')}
-                />
+                <TextInput label='Title' placeholder='Title' {...taskForm.getInputProps('title')} />
+                <Input.Wrapper>
+                    <Input.Label mb='xs'>
+                        <Group align='center'>
+                            <Box>Content</Box>
+                            <Switch
+                                label='Preview'
+                                checked={previewMode}
+                                onChange={(event) => {
+                                    setPreviewMode(event.currentTarget.checked);
+                                }}
+                                sx={{
+                                    display: 'flex',
+                                }}
+                            />
+                        </Group>
+                    </Input.Label>
+                    <Paper
+                        dangerouslySetInnerHTML={{
+                            __html: editor?.getHTML() || '',
+                        }}
+                        withBorder
+                        p={15}
+                        display={previewMode ? 'block' : 'none'}
+                    />
+                    <Box display={previewMode ? 'none' : 'block'}>
+                        <RTE editor={editor} />
+                    </Box>
+                </Input.Wrapper>
                 <ColorInput
                     format='rgb'
                     placeholder='Pick a Color'
@@ -80,9 +140,7 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
                         firstDayOfWeek='sunday'
                         clearable={false}
                         maxDate={
-                            taskForm.values.finishDate
-                                ? taskForm.values.finishDate
-                                : undefined
+                            taskForm.values.finishDate ? taskForm.values.finishDate : undefined
                         }
                         withinPortal
                         {...taskForm.getInputProps('startDate')}
@@ -96,11 +154,7 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
                         label='Finish Date'
                         firstDayOfWeek='sunday'
                         clearable={false}
-                        minDate={
-                            taskForm.values.startDate
-                                ? taskForm.values.startDate
-                                : undefined
-                        }
+                        minDate={taskForm.values.startDate ? taskForm.values.startDate : undefined}
                         withinPortal
                         {...taskForm.getInputProps('finishDate')}
                     />
@@ -115,13 +169,10 @@ const Form = ({ onSubmit, initialValues, loading }: Props) => {
                     checked={taskForm.values.done}
                     {...taskForm.getInputProps('done')}
                 />
-                <Center>
-                    <Button type='submit' uppercase loading={loading}>
-                        Submit
-                    </Button>
-                </Center>
+                {formBottomSection}
             </Stack>
         </form>
     );
 };
+
 export default Form;
---
+++ src/core/task/components/RTE.tsx
@@ -0,0 +1,85 @@
+import { RichTextEditor } from '@mantine/tiptap';
+import { Editor } from '@tiptap/core';
+import { BubbleMenu } from '@tiptap/react';
+
+const RTEControls = () => {
+    return (
+        <>
+            <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Bold />
+                <RichTextEditor.Italic />
+                <RichTextEditor.Underline />
+                <RichTextEditor.Strikethrough />
+                <RichTextEditor.ClearFormatting />
+                <RichTextEditor.Highlight />
+                <RichTextEditor.Code />
+                <RichTextEditor.ColorPicker
+                    colors={[
+                        '#25262b',
+                        '#868e96',
+                        '#fa5252',
+                        '#e64980',
+                        '#be4bdb',
+                        '#7950f2',
+                        '#4c6ef5',
+                        '#228be6',
+                        '#15aabf',
+                        '#12b886',
+                        '#40c057',
+                        '#82c91e',
+                        '#fab005',
+                        '#fd7e14',
+                    ]}
+                />
+            </RichTextEditor.ControlsGroup>
+
+            <RichTextEditor.ControlsGroup>
+                <RichTextEditor.H1 />
+                <RichTextEditor.H2 />
+                <RichTextEditor.H3 />
+                <RichTextEditor.H4 />
+            </RichTextEditor.ControlsGroup>
+
+            <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Blockquote />
+                <RichTextEditor.Hr />
+                <RichTextEditor.BulletList />
+                <RichTextEditor.OrderedList />
+                <RichTextEditor.Subscript />
+                <RichTextEditor.Superscript />
+            </RichTextEditor.ControlsGroup>
+
+            <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Link />
+                <RichTextEditor.Unlink />
+            </RichTextEditor.ControlsGroup>
+
+            <RichTextEditor.ControlsGroup>
+                <RichTextEditor.AlignLeft />
+                <RichTextEditor.AlignCenter />
+                <RichTextEditor.AlignJustify />
+                <RichTextEditor.AlignRight />
+            </RichTextEditor.ControlsGroup>
+        </>
+    );
+};
+
+interface Props {
+    editor: Editor | null;
+}
+
+const RTE = ({ editor }: Props) => {
+    return (
+        <RichTextEditor editor={editor}>
+            <RichTextEditor.Toolbar>
+                <RTEControls />
+            </RichTextEditor.Toolbar>
+            {editor && <BubbleMenu editor={editor}>
+                <RTEControls />
+            </BubbleMenu>}
+            <RichTextEditor.Content />
+        </RichTextEditor>
+    );
+};
+
+export default RTE;
