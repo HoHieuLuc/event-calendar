6c000d8e480cfcc9058cecc5eb23e5ce0c6e7f3c
Ho Hieu Luc <kingofika123@gmail.com>
2022-11-29 19:10:15 +0700

    update cache time to infinity

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -3,8 +3,8 @@ import { QueryClient } from '@tanstack/react-query';
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 1000 * 60 * 5,
-            cacheTime: 1000 * 60 * 5,
+            staleTime: Infinity,
+            cacheTime: Infinity,
             refetchOnWindowFocus: false,
             retry: false,
         },
